3738848513c83a86b245b8c849e36f71c814622a
Carlos Henrique <ichfs9108@gmail.com>
2024-07-09 21:48:08 -0300

    feat: Anotacoes JPA para classes de dominio

--- helpdesk/src/main/java/com/valdir/helpdesk/domain/Tecnico.java
+++ helpdesk/src/main/java/com/valdir/helpdesk/domain/Tecnico.java
@@ -1,17 +1,26 @@
 package com.valdir.helpdesk.domain;
 
+import com.valdir.helpdesk.domain.enums.Perfil;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Tecnico extends Pessoa {
 
+    @OneToMany(mappedBy = "tecnico")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Tecnico() {
+        addPerfil(Perfil.CLIENTE);
     }
 
     public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+
+        addPerfil(Perfil.CLIENTE);
     }
 
     public List<Chamado> getChamados() {
