02dbda9f71ffdbae6007a50dacd4e61c390d8bf6
Carlos Henrique <ichfs9108@gmail.com>
2024-07-11 17:48:18 -0300

    feat: Endpoint findById para tecnico

--- helpdesk/src/main/java/com/valdir/helpdesk/HelpdeskApplication.java
+++ helpdesk/src/main/java/com/valdir/helpdesk/HelpdeskApplication.java
@@ -1,49 +1,13 @@
 package com.valdir.helpdesk;
 
-import com.valdir.helpdesk.domain.Chamado;
-import com.valdir.helpdesk.domain.Cliente;
-import com.valdir.helpdesk.domain.Tecnico;
-import com.valdir.helpdesk.domain.enums.Perfil;
-import com.valdir.helpdesk.domain.enums.Prioridade;
-import com.valdir.helpdesk.domain.enums.Status;
-import com.valdir.helpdesk.repositories.ChamadoRepository;
-import com.valdir.helpdesk.repositories.ClienteRepository;
-import com.valdir.helpdesk.repositories.TecnicoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-
 @SpringBootApplication
-public class HelpdeskApplication implements CommandLineRunner {
-
-    @Autowired
-    private ClienteRepository clienteRepository;
-
-    @Autowired
-    private TecnicoRepository tecnicoRepository;
-
-    @Autowired
-    private ChamadoRepository chamadoRepository;
+public class HelpdeskApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelpdeskApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-
-        Tecnico tec1 = new Tecnico(null, "Valdir Cezar", "63653230268", "valdir@gmail.com", "123");
-        tec1.addPerfil(Perfil.ADMIN);
-
-        Cliente cli1 = new Cliente(null, "Linus Torvalds", "80527954780", "torvalds@gmail.com", "123");
-
-        Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado", tec1, cli1);
-
-        tecnicoRepository.saveAll(Arrays.asList(tec1));
-        clienteRepository.saveAll(Arrays.asList(cli1));
-        chamadoRepository.saveAll(Arrays.asList(c1));
-    }
 }
