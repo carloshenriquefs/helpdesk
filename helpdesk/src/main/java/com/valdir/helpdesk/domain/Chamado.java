de74b988b0d9ed3bdefb819d1c14e31fe1ac8a54
Carlos Henrique <ichfs9108@gmail.com>
2024-07-12 23:29:53 -0300

    feat: Implementando Serializable

--- helpdesk/src/main/java/com/valdir/helpdesk/domain/Chamado.java
+++ helpdesk/src/main/java/com/valdir/helpdesk/domain/Chamado.java
@@ -10,6 +10,7 @@ import java.time.LocalDate;
 
 @Entity
 public class Chamado implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- helpdesk/src/main/java/com/valdir/helpdesk/domain/Pessoa.java
+++ helpdesk/src/main/java/com/valdir/helpdesk/domain/Pessoa.java
@@ -12,6 +12,7 @@ import java.util.stream.Collectors;
 
 @Entity
 public abstract class Pessoa implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
