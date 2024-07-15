b67a6eb15ba95fe34feb6d546fdbd3b0c63b0a60
Carlos Henrique <ichfs9108@gmail.com>
2024-07-15 01:25:48 -0300

    feat: findAll para Chamado

--- helpdesk/src/main/java/com/valdir/helpdesk/resources/ChamadoResource.java
+++ helpdesk/src/main/java/com/valdir/helpdesk/resources/ChamadoResource.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/chamados")
@@ -23,4 +25,10 @@ public class ChamadoResource {
         return ResponseEntity.ok().body(new ChamadoDTO(obj));
     }
 
+    @GetMapping
+    public ResponseEntity<List<ChamadoDTO>> findAll() {
+        List<Chamado> list = chamadoService.findAll();
+        List<ChamadoDTO> listDTO = list.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
+    }
 }
